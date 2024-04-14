fd8792a487696a2c1556ce807c486fd68d9655d0
Ibe Augustine <aibe64@yahoo.com>
2024-04-14 11:43:57 +0100

    writing scripts for sinle hooks with fetch

--- src/hooks/useFetchSingleUser.ts
+++ src/hooks/useFetchSingleUser.ts
@@ -1,46 +1,46 @@
-// /* eslint-disable react-hooks/exhaustive-deps */
-// /**
-//  *
-//  * This hook help get a single user record from the table when the view details is clicked
-//  *
-//  */
+/* eslint-disable react-hooks/exhaustive-deps */
+/**
+ *
+ * This hook help get a single user record from the table when the view details is clicked
+ *
+ */
 
-// import { useAppDispatch, useAppSelector } from "../store/hooks";
-// import { Apiresponse } from "../model/client/response";
-// import { setAllAppKeys } from "../store";
-// import { useCallback, useEffect } from "react";
-// import { useLocation } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { Apiresponse } from "../model/client/response";
+import { setAllAppKeys } from "../store";
+import { useCallback, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-// const useFetchSignleUser = (): void => {
-//   const dispatch = useAppDispatch();
-//   const state = useAppSelector((state) => {
-//     return state.app;
-//   });
+const useFetchSignleUser = (): void => {
+  const dispatch = useAppDispatch();
+  const state = useAppSelector((state) => {
+    return state.app;
+  });
 
-//   const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-//   const userId = pathname.split("/")[3];
+  const userId = pathname.split("/")[3];
 
-//   const onGetRecord = useCallback(() => {
-//     const users: Array<Apiresponse.Users> = JSON.parse(
-//       localStorage.getItem("users") as string
-//     );
-//     if (users && users.length > 0) {
-//       const singleUser = users.find((user) => {
-//         return user._id === userId;
-//       });
-//       dispatch(
-//         setAllAppKeys({
-//           ...state,
-//           record: singleUser,
-//         })
-//       );
-//     }
-//   }, [dispatch, state.users, userId]);
+  const onGetRecord = useCallback(() => {
+    const users: Array<Apiresponse.Users> = JSON.parse(
+      localStorage.getItem("users") as string
+    );
+    if (users && users.length > 0) {
+      const singleUser = users.find((user) => {
+        return user._id === userId;
+      });
+      dispatch(
+        setAllAppKeys({
+          ...state,
+          record: singleUser,
+        })
+      );
+    }
+  }, [dispatch, state.users, userId]);
 
-//   useEffect(() => {
-//     onGetRecord()
-//   }, [onGetRecord]);
-// };
+  useEffect(() => {
+    onGetRecord()
+  }, [onGetRecord]);
+};
 
-// export default useFetchSignleUser;
+export default useFetchSignleUser;
