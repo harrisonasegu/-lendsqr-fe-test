6488788ac8b0d1b5e5eb1bba4fba4e19ee6c7c33
Ibe Augustine <aibe64@yahoo.com>
2024-04-14 11:47:45 +0100

    notification of login updated

--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -1,45 +1,45 @@
-// import { App } from "antd";
+import { App } from "antd";
 
-// type NotificationType = "info" | "success" | "error";
+type NotificationType = "info" | "success" | "error";
 
-// interface NotificationResult {
-//   onNotify: (type: NotificationType, description: string, message?: string) => void;
-// }
+interface NotificationResult {
+  onNotify: (type: NotificationType, description: string, message?: string) => void;
+}
 
-// const useNotification = (): NotificationResult => {
-//   const { notification } = App.useApp();
+const useNotification = (): NotificationResult => {
+  const { notification } = App.useApp();
 
-//   const onNotify = (
-//     type: NotificationType,
-//     description: string,
-//     message?: string
-//   ) => {
-//     switch (type) {
-//       case "info":
-//         notification.info({
-//           message,
-//           description,
-//           placement: "topRight",
-//         });
-//         break;
-//       case "success":
-//         notification.success({
-//           message,
-//           description,
-//           placement: "topRight",
-//         });
-//         break;
-//       default:
-//         notification.error({
-//             message,
-//             description,
-//             placement: "topRight",
-//           });
-//         break;
-//     }
-//   };
+  const onNotify = (
+    type: NotificationType,
+    description: string,
+    message?: string
+  ) => {
+    switch (type) {
+      case "info":
+        notification.info({
+          message,
+          description,
+          placement: "topRight",
+        });
+        break;
+      case "success":
+        notification.success({
+          message,
+          description,
+          placement: "topRight",
+        });
+        break;
+      default:
+        notification.error({
+            message,
+            description,
+            placement: "topRight",
+          });
+        break;
+    }
+  };
 
-//   return { onNotify };
-// };
+  return { onNotify };
+};
 
-// export default useNotification;
+export default useNotification;
