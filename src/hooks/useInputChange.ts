3d1d6f701e45b26112b63400414832ddcf4d504c
Ibe Augustine <aibe64@yahoo.com>
2024-04-14 11:45:45 +0100

    Updating of table data inputs and filter

--- src/hooks/useInputChange.ts
+++ src/hooks/useInputChange.ts
@@ -1,39 +1,39 @@
-// /**
-//  *
-//  * This custom hook is created to handle the collection of data entered by the user in any input field
-//  *
-//  */
-// import { useCallback } from "react";
-// import { useAppDispatch, useAppSelector } from "../store/hooks";
-// import { setAllAppKeys } from "../store";
+/**
+ *
+ * This custom hook is created to handle the collection of data entered by the user in any input field
+ *
+ */
+import { useCallback } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { setAllAppKeys } from "../store";
 
-// interface InputChangeFunction {
-//   (key: string, value: unknown): void;
-// }
+interface InputChangeFunction {
+  (key: string, value: unknown): void;
+}
 
-// const useInputChange = (): InputChangeFunction => {
-//   const dispatch = useAppDispatch();
-//   const state = useAppSelector((state) => {
-//     return state.app;
-//   });
+const useInputChange = (): InputChangeFunction => {
+  const dispatch = useAppDispatch();
+  const state = useAppSelector((state) => {
+    return state.app;
+  });
 
-//   //   This function is used to collect user entered data in input fields
-//   const onFieldChange = useCallback(
-//     (key: string, value: unknown) => {
-//       dispatch(
-//         setAllAppKeys({
-//           ...state,
-//           request: {
-//             ...state.request,
-//             [key]: value,
-//           },
-//         })
-//       );
-//     },
-//     [dispatch, state]
-//   );
+  //   This function is used to collect user entered data in input fields
+  const onFieldChange = useCallback(
+    (key: string, value: unknown) => {
+      dispatch(
+        setAllAppKeys({
+          ...state,
+          request: {
+            ...state.request,
+            [key]: value,
+          },
+        })
+      );
+    },
+    [dispatch, state]
+  );
 
-//   return onFieldChange;
-// };
+  return onFieldChange;
+};
 
-// export default useInputChange;
+export default useInputChange;
