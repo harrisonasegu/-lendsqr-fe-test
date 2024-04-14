88809b3da74c5d07dfc151061c6ff0159c6a5deb
Ibe Augustine <aibe64@yahoo.com>
2024-04-14 11:47:06 +0100

    Login page updated

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,35 +1,35 @@
-// import { useCallback } from "react";
-// import { useAppSelector } from "../store/hooks";
-// import { useNavigate } from "react-router-dom";
-// import { routePath, storeDataToLocalStorage } from "../utils/helper";
-// import useNotification from "./useNotification";
+import { useCallback } from "react";
+import { useAppSelector } from "../store/hooks";
+import { useNavigate } from "react-router-dom";
+import { routePath, storeDataToLocalStorage } from "../utils/helper";
+import useNotification from "./useNotification";
 
-// interface LoginResult {
-//   onLogin: () => void;
-// }
+interface LoginResult {
+  onLogin: () => void;
+}
 
-// const useLogin = (): LoginResult => {
-//   const state = useAppSelector((state) => {
-//     return state.app;
-//   });
+const useLogin = (): LoginResult => {
+  const state = useAppSelector((state) => {
+    return state.app;
+  });
 
-//   const navigate = useNavigate();
-//   const { onNotify } = useNotification();
+  const navigate = useNavigate();
+  const { onNotify } = useNotification();
 
-//   const onLogin = useCallback(() => {
+  const onLogin = useCallback(() => {
     
-//       onNotify("success", "Login is successful", "Authenticated");
-//       storeDataToLocalStorage()
-//       localStorage.setItem("***", btoa(state.request?.password))
-//       setTimeout(() => {
-//         return navigate(routePath.User, {
-//           replace: true,
-//         });
-//       }, 500);
+      onNotify("success", "Login is successful", "Authenticated");
+      storeDataToLocalStorage()
+      localStorage.setItem("***", btoa(state.request?.password))
+      setTimeout(() => {
+        return navigate(routePath.User, {
+          replace: true,
+        });
+      }, 500);
     
-//   }, [navigate, onNotify, state.request?.password]);
+  }, [navigate, onNotify, state.request?.password]);
 
-//   return { onLogin };
-// };
+  return { onLogin };
+};
 
-// export default useLogin;
+export default useLogin;
