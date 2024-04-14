86e1a3e4933b858715d716835164b51b0c48a469
Ibe Augustine <aibe64@yahoo.com>
2024-04-14 11:54:26 +0100

    updating routes

--- src/route/lazy-routes.ts
+++ src/route/lazy-routes.ts
@@ -1,7 +1,7 @@
-// import { lazy } from "react";
+import { lazy } from "react";
 
-// const Login = lazy(() => import("../views/Login/Login"));
-// const User = lazy(() => import("../views/User/User"));
-// const UserDetails = lazy(() => import("../views/User/UserDetails"));
+const Login = lazy(() => import("../views/Login/Login"));
+const User = lazy(() => import("../views/User/User"));
+const UserDetails = lazy(() => import("../views/User/UserDetails"));
 
-// export { Login, User, UserDetails };
+export { Login, User, UserDetails };
